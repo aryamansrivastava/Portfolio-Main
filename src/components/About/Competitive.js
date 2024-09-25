33b2a0043d091d0f8118d1f18dd51f3223820902
aryamansrivastava <srivastavaaryaman6@gmail.com>
2024-09-26 00:14:26 +0530

    Update competitive.js

--- src/components/About/Competitive.js
+++ src/components/About/Competitive.js
@@ -10,13 +10,19 @@ function Competitive() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiCodechef />
+        <a href="https://www.codechef.com/users/aryaman_sri" target="_blank" rel="noopener noreferrer">
+          <SiCodechef style={{ color: "white" }} />
+        </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiCodeforces />
+        <a href="https://codeforces.com/profile/aryaman_sri" target="_blank" rel="noopener noreferrer">
+          <SiCodeforces style={{ color: "white" }} />
+        </a>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLeetcode />
+        <a href="https://leetcode.com/u/aryamansrivastava/" target="_blank" rel="noopener noreferrer">
+          <SiLeetcode style={{ color: "white" }} />
+        </a>
       </Col>
     </Row>
   );
