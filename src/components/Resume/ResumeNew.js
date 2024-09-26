87ae2a12b5a63aa3dd9515f38ad73bf84045346b
aryamansrivastava <srivastavaaryaman6@gmail.com>
2024-09-26 20:15:34 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/ARYAMAN_Resume.pdf";
+import pdf from "../../Assets/Aryaman_Srivastava_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -58,18 +58,6 @@ function ResumeNew() {
           </Document>
         </Row>
 
-        {/* Duplicate download button */}
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
       </Container>
     </div>
   );
