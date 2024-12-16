1da8dda95f76a9c1c45db4ff3a62e2fb84040de2
Aryaman Srivastava <115610445+aryamansrivastava@users.noreply.github.com>
2024-12-17 03:14:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/aryaman_sri/?hl=en"
+                href="https://www.instagram.com/aryaman__srivastava/?hl=en"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
