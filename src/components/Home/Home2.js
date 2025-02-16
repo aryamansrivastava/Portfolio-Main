1aea3f961c259783cee09ff39b7ffe9e49bd4342
aryamansrivastava <srivastavaaryaman6@gmail.com>
2025-02-16 19:11:06 +0530

    udpated protfolio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -136,7 +136,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/aryaman_sri/"
+                  href="https://www.instagram.com/aryaman__srivastava/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
